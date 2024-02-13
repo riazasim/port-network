dee0d23322e6ba5e77939d335c3d624f10a8c075
riazasim <riazasim156@gmail.com>
2024-02-13 16:18:56 +0500

    changes

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ui-truck-app'),
+      dir: require('path').join(__dirname, './coverage/ui-port-network-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
