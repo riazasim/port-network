bf85181353ba8ebbd66a104cc071afc5e027a7b4
riazasim <riazasim156@gmail.com>
2024-02-21 17:10:47 +0500

    search-card

--- src/app/admin/map/search/search.component.ts
+++ src/app/admin/map/search/search.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
 import { CompanyModel } from 'src/app/core/models/company.model';
-
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CompanyService } from 'src/app/core/services/company.service';
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrl: './search.component.scss'
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrl: './search.component.scss'
 })
 export class SearchComponent {
-  isActive: number = 1;
-  companies: CompanyModel[] = [];
-  onImgClick(number : number){
-    this.isActive = number
-  }
+    constructor(
+        private readonly companyService: CompanyService,
+        private readonly cd: ChangeDetectorRef) {
+            this.retrieveCompanies();
+         }
+
+    isActive: number = 1;
+    companies: CompanyModel[] = [];
+
+    onImgClick(number: number) {
+        this.isActive = number
+    }
+
+    retrieveCompanies(): void {
+
+        let data={
+            "start": 0,
+            "length": 0,
+            "filters": ["","","","",""],
+            "order": [{"dir": "DESC", "column": 0}]
+        }
+        this.companyService.pagination(data).subscribe(response => {
+            this.companies = response.items;
+            this.cd.detectChanges();
+        })
+    }
 }
