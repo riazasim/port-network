50f620588b44dded22f1324fc2642713acbf4cb3
asim <riazasim156@gmail.com>
2024-05-03 17:07:03 +0500

    'fixes'

--- src/app/admin/companies/companies-add-edit/companies-add-edit.component.ts
+++ src/app/admin/companies/companies-add-edit/companies-add-edit.component.ts
@@ -18,17 +18,18 @@ export class CompaniesAddEditComponent implements OnInit {
   isLoading$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
   stepOne$: BehaviorSubject<boolean> = new BehaviorSubject(false);
   @ViewChild('stepper', { static: false }) matStepper: MatStepper;
-  company$: BehaviorSubject<CompanyModel|null> = new BehaviorSubject<CompanyModel|null>(null);
+  company$: BehaviorSubject<CompanyModel | null> = new BehaviorSubject<CompanyModel | null>(null);
   id: number;
   portId: number | null;
   ports: PortModel[] = [];
+  // options: number;
   isLinear = true;
   constructor(private fb: UntypedFormBuilder,
-              private companyService: CompanyService,
-              private readonly portService: PortService,
-              private router: Router,
-              private route: ActivatedRoute,
-              private readonly cd: ChangeDetectorRef) { }
+    private companyService: CompanyService,
+    private readonly portService: PortService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private readonly cd: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.subscribeForQueryParams();
@@ -39,14 +40,14 @@ export class CompaniesAddEditComponent implements OnInit {
     if (this.matStepper) {
       this.matStepper.selectedIndex = index;
     }
-}
+  }
 
   subscribeForQueryParams(): void {
     this.id = this.route.snapshot.params['id'];
     if (this.id) {
       this.companyService.get(this.id).subscribe(async response => {
         this.initForm(response);
-        this.company$.next({...response })
+        this.company$.next({ ...response })
         this.isLoading$.next(false);
       });
     } else {
@@ -85,72 +86,82 @@ export class CompaniesAddEditComponent implements OnInit {
       imgPreview: this.fb.control(data?.imgPreview || ''),
     });
 
-  if (data.contacts) {
-    data.contacts.forEach(contact => {
-      this.addContact(contact);
-    });
-  }
-}
-
-get contacts(): any {
-  return this.companyForm.get('contacts');
-}
-
-addContact(contact?: ContactsModel): void {
-  const newContact = this.fb.group({
-    name: [contact?.name || '', Validators.required],
-    capacity: [contact?.capacity || '', Validators.required],
-    occupiedCapacity: [contact?.occupiedCapacity || '', Validators.required],
-    client: [contact?.client || '', Validators.required],
-    product: [contact?.product || '', Validators.required],
-  });
-  this.contacts.push(newContact);
-}
-
-removeContact(index: number): void {
-  const companyId = this.company$?.value?.id;
-  const contactsArray = this.company$?.value?.contacts;
-
-  if (companyId && contactsArray && contactsArray.length > 0) {
-    const contactId = contactsArray[index]?.id; // Get the ID of the contact at the specified index
-    if (contactId !== null && contactId !== undefined && contactId > -1) {
-      this.companyService.deleteContact(companyId, contactId).subscribe({
-        next: () => {
-          console.log('Contact deleted successfully');
-          this.contacts.removeAt(index); // Remove the contact from the form array
-          this.cd.detectChanges();
-          return this.companyForm.get('contacts');
-        },
-        error: error => {
-          console.error('Error deleting contact:', error);
-          // Handle error, such as showing an error message to the user
-        }
+    if (data.contacts) {
+      data.contacts.forEach(contact => {
+        this.addContact(contact);
       });
     }
-  } else {
-    this.contacts.removeAt(index);
   }
-}
 
-onPortChange(value: any) {
-  this.subscribeForPortChanges(value.target.value);
-} 
+  get contacts(): any {
+    return this.companyForm.get('contacts');
+  }
 
-subscribeForPortChanges(data: any): void {
-  this.companyForm.get('portId')?.setValue(data);
-}
+  // onOptionChange(ev: any) {
+  //   this.options = -1;
+  //   this.options = ev;
 
-retrievePorts() {
-  this.portService.pagination({
-    "start": 0,
-    "length": 0,
-    "filters": ["", "", "", "", "", "", "", "", ""],
-    "order": [{ "dir": "DESC", "column": 0 }]
-  }).subscribe(response => {
-    this.ports = response.items
-    this.cd.detectChanges();
-  })
-}
+  //   if(this.options === 1){
+  //     this.companyForm.patchValue({isTypeShipOwner : true , isTypeAgentCompany : false})
+  //   }
+
+  // }
+
+  addContact(contact?: ContactsModel): void {
+    const newContact = this.fb.group({
+      name: [contact?.name || '', Validators.required],
+      capacity: [contact?.capacity || '', Validators.required],
+      occupiedCapacity: [contact?.occupiedCapacity || '', Validators.required],
+      client: [contact?.client || '', Validators.required],
+      product: [contact?.product || '', Validators.required],
+    });
+    this.contacts.push(newContact);
+  }
+
+  removeContact(index: number): void {
+    const companyId = this.company$?.value?.id;
+    const contactsArray = this.company$?.value?.contacts;
+
+    if (companyId && contactsArray && contactsArray.length > 0) {
+      const contactId = contactsArray[index]?.id; // Get the ID of the contact at the specified index
+      if (contactId !== null && contactId !== undefined && contactId > -1) {
+        this.companyService.deleteContact(companyId, contactId).subscribe({
+          next: () => {
+            console.log('Contact deleted successfully');
+            this.contacts.removeAt(index); // Remove the contact from the form array
+            this.cd.detectChanges();
+            return this.companyForm.get('contacts');
+          },
+          error: error => {
+            console.error('Error deleting contact:', error);
+            // Handle error, such as showing an error message to the user
+          }
+        });
+      }
+    } else {
+      this.contacts.removeAt(index);
+    }
+  }
+
+  onPortChange(value: any) {
+    this.subscribeForPortChanges(value.target.value);
+  }
+
+  subscribeForPortChanges(data: any): void {
+    this.companyForm.get('portId')?.setValue(data);
+  }
+
+  retrievePorts() {
+    this.portService.pagination({
+      "start": 0,
+      "length": 0,
+      "filters": ["", "", "", "", "", "", "", "", ""],
+      "order": [{ "dir": "DESC", "column": 0 }]
+    }).subscribe(response => {
+      this.ports = response.items
+      this.cd.detectChanges();
+    })
+  }
 
   setImgPreview(target: any, input: any): void {
     if (target.files.item(0)) {
@@ -171,7 +182,7 @@ retrievePorts() {
   }
 
   parseData(company: any): any {
-    const data = {...company};
+    const data = { ...company };
     if (!data.imgPreview) {
       delete data.imgPreview;
     }
